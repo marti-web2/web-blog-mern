21fb5719b383acb3384a47b9d9fbce1852fb87b1
Marti <marti@martimcdee.com>
2022-12-28 23:38:28 +0000

    added 'create a new post' component

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React, { useState } from "react"
 import Topbar from "./components/topbar/Topbar"
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 const App = () => {
 
   return (
     <>
       <Topbar />
-     <Single />
+     <Write />
     </>
   );
 };
