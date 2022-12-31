7936220c7bfaa5e6d46c40027d0dd677bf51c8a8
David Martinez <anddagger@gmail.com>
2022-12-31 00:10:43 -0600

    implemented frontend routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,26 @@ import Single from "./pages/single/Single"
 import Write from "./pages/write/Write"
 import Settings from "./pages/settings/Settings"
 import Register from "./pages/register/Register"
+import Login from "./pages/login/Login"
+import About from "./pages/about/About"
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
 
 const App = () => {
+  const user = false
 
   return (
-    <Fragment>
-      <Topbar />
-      <Register />
-    </Fragment>
+      <Router>
+        <Topbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/register" element={user ? <Home /> : <Register />} />
+          <Route path="/login" element={user ? <Home /> : <Login />} />
+          <Route path="/write" element={user ? <Write /> : <Register />} />
+          <Route path="/settings" element={user ? <Settings /> : <Register />} />
+          <Route path="/post/:postId" element={<Single />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
   );
 };
 
---
+++ client/src/pages/about/About.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "./about.css"
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Welcome to Marti's blog! My name is Marti, and I am a freelance developer based in Texas. On this blog, you will
+        find posts about all things code, music, and art related. Whether you're a beginner or a seasoned developer, I
+        hope you find something here that interests you. Thanks for stopping by!
+      </p>
+    </div>
+  )
+}
